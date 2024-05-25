15ab5bdc4c528cfeedc169773ed64c57371ad97e
Nicholas Walvoord <nwlvrd@umich.edu>
2024-05-25 16:26:54 -0500

    Updates

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,19 @@ export default function About() {
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-              Hi, I'm Nick.
-              <br className="hidden lg:inline-block" />I love to build amazing
-              apps.
+              Hi, I'm Nick
+              <br className="hidden lg:inline-block" />I love to solve complex problems.
             </h1>
             <p className="mb-8 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-              laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-              Laborum, voluptas natus?
+              I'm an outgoing and collaborative student at the University of Michigan, currently studying computer science and math. 
+              Currently, I'm a software engineer intern at Puppod, a software analyst at Atlas Digital Consulting, 
+              and Director of CS Program at Michigan Mentors.  
             </p>
             <div className="flex justify-center">
               <a
                 href="#contact"
                 className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                Work With Me
+                Contact Me
               </a>
               <a
                 href="#projects"
--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Application and Recruitment Portal",
+    subtitle: "React, TailwindCSS, Firebase, Firebase Authentication, Firestore",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Full-stack web application built for club recruitment. Implemented 20+ dynamic front-end elements and a back-end scaling to 200+ applicants. Code availible upon request.",
     image: "./project-1.gif",
     link: "https://reactbootcamp.com",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Saphran Inc Data Dashboard",
+    subtitle: "JavaScript, HTML/CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Architected responsive data visualization dashboard tailored for asset planning and long-term profit forcasting. Improved menu system filtering over 30,000+ lines of compony data.",
     image: "./project-2.gif",
     link: "https://reactbootcamp.com",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Michigan Mentors",
+    subtitle: "Director of CS Program",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Directing and managing our pro-bono intro to computer science program. More details at https://www.mzbtutors.com/michigan-mentors",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://www.mzbtutors.com/michigan-mentors",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Machience Learning Forum Post Classifier",
+    subtitle: "C++",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Designed and trained naïve Bayes machine learning classifier that reads and processes student posts via CSV files, calculates log-probability score of each post, and predicts labels for each given Piazza post with 87% accuracy (n=3000). Code availible upon request.",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/tdvalencia/p5-ml",
   },
 ];
 
@@ -52,9 +52,12 @@ export const testimonials = [
 
 export const skills = [
   "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
+  "React.js",
+  "C++",
+  "TailwindCSS",
+  "HTML/CSS",
+  "Firebase & Firebase Authentication",
+  "Julia",
+  "Firestore",
+  "MATLAB", 
 ];
