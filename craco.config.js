8b5d94e27efa8a9fe55ad72c4893f8aa66876b5c
Nicholas Walvoord <nwlvrd@umich.edu>
2025-02-03 00:31:02 -0500

    Buid fixes (hopefully)

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,21 @@ module.exports = {
         require('tailwindcss'),
         require('autoprefixer'),
       ],
+    }
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      // Target the Babel loader rule
+      const babelRule = webpackConfig.module.rules.find(rule =>
+        rule.oneOf?.some(r => r.loader?.includes('babel-loader'))
+      );
+      
+      if (babelRule) {
+        // Include react-icons in Babel transpilation
+        babelRule.exclude = /node_modules\/(?!(react-icons)\/).*/;
+      }
+      
+      return webpackConfig;
     },
   },
-}
+};
